4d3f368149bfe7a87fa1a47b1a8c0e48873ffa93
Portponky <portponky@gmail.com>
2026-09-10 12:11:48 +0100

    Split workflow into linux/windows versions

---
+++ export/package-linux.sh
@@ -0,0 +1,10 @@
+rm -f ap-gen-tool-linux.zip
+cd linux
+mkdir -p games
+mkdir -p data
+mkdir -p wads
+mkdir -p output
+rm -f ap-gen-tool-linux.zip
+zip ap-gen-tool-linux.zip ap_gen_tool.sh ap_gen_tool.x86_64 ap_gen_tool.pck games data wads output
+mv ap-gen-tool-linux.zip ..
+
---
+++ export/package-windows.sh
@@ -0,0 +1,10 @@
+rm -f ap-gen-tool-windows.zip
+cd windows
+mkdir -p games
+mkdir -p data
+mkdir -p wads
+mkdir -p output
+rm -f ap-gen-tool-windows.zip
+zip ap-gen-tool-windows.zip ap_gen_tool.exe ap_gen_tool.pck games data wads output
+mv ap-gen-tool-windows.zip ..
+
